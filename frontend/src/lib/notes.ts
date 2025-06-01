54a18be35122cb21882e64ccde8488852b5d6c26
MeetBagda <bagdameet@gmail.com>
2025-06-01 11:56:16 +0530

    Delete : frontend - remove unnecessary files in lib folder

--- frontend/src/lib/notes.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from './client';
-import { Note } from '@/types/note';
-
-export const noteApi = {
-  getNotes: () => apiClient.get<Note[]>('/notes'),
-  createNote: (note: Note) => apiClient.post<Note>('/notes', note),
-  updateNote: (id: number, note: Note) => apiClient.put<Note>(`/notes/${id}`, note),
-  deleteNote: (id: number) => apiClient.delete(`/notes/${id}`)
-};
--- frontend/src/lib/projects.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from './client';
-import { Project } from '@/types/project';
-
-export const projectApi = {
-  getProjects: () => apiClient.get<Project[]>('/projects'),
-  createProject: (project: Project) => apiClient.post<Project>('/projects', project),
-  updateProject: (id: number, project: Project) => apiClient.put<Project>(`/projects/${id}`, project),
-  deleteProject: (id: number) => apiClient.delete(`/projects/${id}`)
-};
