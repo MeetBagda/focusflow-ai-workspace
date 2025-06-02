e4e8692f15bbe8517771392e2e257cfcad751081
MeetBagda <bagdameet@gmail.com>
2025-06-02 05:37:06 +0530

    remove redundent index file

--- frontend/src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-
-import { Navigate } from "react-router-dom";
-
-const Index = () => {
-  // We don't need this file as we've set up direct routing in App.tsx
-  return <Navigate to="/" replace />;
-};
-
-export default Index;
